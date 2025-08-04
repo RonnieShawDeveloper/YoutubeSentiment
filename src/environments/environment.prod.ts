3024b37eba95c7345c36709c4272cfc76e469e68
Ronnie Shaw <rdspromo@gmail.com>
2025-08-04 12:23:17 -0400

    Removed Firebase SDK Settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB3GpSSFltRqD5F-dpW-_W4wxfWZ7h5nbU",
-    authDomain: "r-27e86.firebaseapp.com",
-    projectId: "r-27e86",
-    storageBucket: "r-27e86.firebasestorage.app",
-    messagingSenderId: "740762737580",
-    appId: "1:740762737580:web:42a7f3bb884fab6c0c3809",
-    measurementId: "G-74D6F0C3W6"
+    // Paste your Firebase SDK settings here
+    // apiKey: "",
+    // authDomain: "",
+    // projectId: "",
+    // storageBucket: "",
+    // messagingSenderId: "",
+    // appId: "",
+    // measurementId: ""
   },
   remoteConfigDefaults: {
     gemini_2_5_flash_key: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB3GpSSFltRqD5F-dpW-_W4wxfWZ7h5nbU",
-    authDomain: "r-27e86.firebaseapp.com",
-    projectId: "r-27e86",
-    storageBucket: "r-27e86.firebasestorage.app",
-    messagingSenderId: "740762737580",
-    appId: "1:740762737580:web:42a7f3bb884fab6c0c3809",
-    measurementId: "G-74D6F0C3W6"
+    // Firebase SDK Setting go here
   },
   remoteConfigDefaults: {
     gemini_2_5_flash_key: '',
